f2cbb2fbec3d669252e164622c540cccecc679d0
89jonand <89jonand@gafe.molndal.se>
2026-03-06 10:31:00 +0000

    1.0

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,10 +6,13 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+     pkgs.python311
+     pkgs.python311Packages.pip
+     pkgs.git
+     pkgs.github-cli
+     pkgs.sqlite
+     pkgs.azure-cli
+     pkgs.openssl
   ];
   # Sets environment variables in the workspace
   env = {};
